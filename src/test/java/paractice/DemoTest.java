2530eae70c9801378135155049d2a40115b0be31
Akhil <mahajanakhil685@gmail.com>
2024-01-31 16:23:14 +0530

    new Demo Test Case Metrhod Is added

---
+++ src/test/java/paractice/DemoTest.java
@@ -0,0 +1,23 @@
+package paractice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void Demo()
+	{
+		System.out.println("demo");
+		System.out.println("demo 2 new Test Case Is added");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
