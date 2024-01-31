8e5b7b6430852d9c0edaf4ed6ee97ef5fdc1cd7f
Akhil <mahajanakhil685@gmail.com>
2024-01-31 16:37:05 +0530

    This Is Test Case No 5 Added By The AT

---
+++ src/test/java/paractice/TestCaseNo_5.java
@@ -0,0 +1,13 @@
+package paractice;
+
+import org.testng.annotations.Test;
+
+public class TestCaseNo_5 {
+
+	@Test
+	public void sampleImplementation()
+	{
+		System.out.println("Hi");
+		System.out.println("Hello");
+	}
+}
