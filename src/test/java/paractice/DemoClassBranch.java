d50eb2485663aaa53219d5f684b7d53dba468ca3
Akhil <mahajanakhil685@gmail.com>
2024-02-01 12:08:10 +0530

    This Is Test Case No 2 For The Branch 2

---
+++ src/test/java/paractice/DemoClassBranch.java
@@ -0,0 +1,12 @@
+package paractice;
+
+import org.junit.Test;
+
+public class DemoClassBranch {
+	@Test
+	public void testmethod()
+	{
+		System.out.println("New Branch is Generated for Test Case No 2");
+	}
+
+}
