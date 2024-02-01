bb4d6ce39b0233790fbde61de73d326b7fb86db6
Akhil <mahajanakhil685@gmail.com>
2024-02-01 11:37:11 +0530

    Branch No 1

--- src/test/java/paractice/TestCaseNo_5.java
+++ src/test/java/paractice/TestCaseNo_5.java
@@ -10,5 +10,7 @@ public class TestCaseNo_5 {
 		System.out.println("Hi");
 		System.out.println("Hello");
 		System.out.println("<--------Branch------->");
+		System.out.println("<--------Branch 1------->");
+		System.out.println("<--------Branch 2------->");
 	}
 }
